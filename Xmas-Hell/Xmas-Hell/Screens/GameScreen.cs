8f3fb104921651ee83524692962f60c065c96808
Noxalus <noxalus@gmail.com>
2016-11-06 18:04:19 +0100

    Add a first version of the Xmas Bell animation.

--- Xmas-Hell/Xmas-Hell/Screens/GameScreen.cs
+++ Xmas-Hell/Xmas-Hell/Screens/GameScreen.cs
@@ -1,9 +1,7 @@
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Media;
 using MonoGame.Extended.Screens;
 using Xmas_Hell.Entities;
-using Xmas_Hell.Entities.Bosses;
 using Xmas_Hell.Entities.Bosses.XmasBall;
 
 namespace Xmas_Hell.Screens
