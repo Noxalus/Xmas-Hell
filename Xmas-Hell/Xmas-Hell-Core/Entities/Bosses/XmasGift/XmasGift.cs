994c0f7f253f19de502bcf666f81c07c0e98ed22
Noxalus <noxalus@gmail.com>
2017-01-22 15:04:01 +0100

    Push missing file to have a debug view of Farseer colliders.

--- Xmas-Hell/Xmas-Hell-Core/Entities/Bosses/XmasGift/XmasGift.cs
+++ Xmas-Hell/Xmas-Hell-Core/Entities/Bosses/XmasGift/XmasGift.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using BulletML;
 using FarseerPhysics.Collision.Shapes;
@@ -9,7 +10,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using XmasHell.Physics.Collision;
 using Xmas_Hell_Core.Controls;
-//using Xmas_Hell_Core.Physics.Debug;
+using Xmas_Hell_Core.Physics.DebugView;
 
 namespace XmasHell.Entities.Bosses.XmasGift
 {
@@ -23,7 +24,30 @@ namespace XmasHell.Entities.Bosses.XmasGift
         private Body _rightWallBody;
         private Body _bottomWallBody;
 
-        //private DebugView _debugView;
+        private Fixture _giftFixture;
+
+        private DebugView _debugView;
+        public override float Width()
+        {
+            if (CurrentAnimator != null)
+            {
+                var spriteBodyPart = Array.Find(CurrentAnimator.Entity.Spriter.Folders[0].Files, (file) => file.Name == "body.png");
+                return spriteBodyPart.Height;
+            }
+
+            return 0f;
+        }
+
+        public override float Height()
+        {
+            if (CurrentAnimator != null)
+            {
+                var spriteBodyPart = Array.Find(CurrentAnimator.Entity.Spriter.Folders[0].Files, (file) => file.Name == "body.png");
+                return spriteBodyPart.Width;
+            }
+
+            return 0f;
+        }
 
         public XmasGift(XmasHell game, PositionDelegate playerPositionDelegate) : base(game, playerPositionDelegate)
         {
@@ -36,9 +60,10 @@ namespace XmasHell.Entities.Bosses.XmasGift
             // Physics
             Game.GameManager.CollisionWorld.AddBossHitBox(new SpriterCollisionCircle(this, "body.png", new Vector2(0f, 10f), 0.90f));
             var gravity = new Vector2(0f, 9.82f);
+
             _world = new World(gravity);
 
-            //_debugView = new DebugView(_world, Game, 1f);
+            _debugView = new DebugView(_world, Game, 1f);
 
             // Behaviours
             Behaviours.Add(new XmasGiftBehaviour1(this, _world));
@@ -56,17 +81,20 @@ namespace XmasHell.Entities.Bosses.XmasGift
             _giftBody = BodyFactory.CreateBody(_world);
             _giftBody.BodyType = BodyType.Dynamic;
             _giftBody.Position = Position();
-            //_giftBody.LinearDamping = 0f;
+            _giftBody.LinearDamping = -10f;
             //_giftBody.AngularDamping = 0f;
             //_giftBody.LinearVelocity = new Vector2(0, 100);
             //_giftBody.Mass = 250f;
 
+            var width = Height();
+            var height = Width();
+
             var giftShape = new PolygonShape(new Vertices(new List<Vector2>
             {
-                new Vector2(0, 0),
-                new Vector2(Width(), 0),
-                new Vector2(Width(), Height()),
-                new Vector2(0, Height())
+                new Vector2(-width / 2f, -height / 2f),
+                new Vector2(width / 2f, -height / 2f),
+                new Vector2(width / 2f, height / 2f),
+                new Vector2(-width / 2f, height / 2f)
             }), 1f);
 
             // Walls
@@ -76,27 +104,27 @@ namespace XmasHell.Entities.Bosses.XmasGift
             _topWallBody = BodyFactory.CreateBody(_world);
 
             var bottomWallShape = new EdgeShape(
-                new Vector2(0, Game.Graphics.GraphicsDevice.Viewport.Height),
-                new Vector2(Game.Graphics.GraphicsDevice.Viewport.Width, Game.Graphics.GraphicsDevice.Viewport.Height)
+                new Vector2(0, GameConfig.VirtualResolution.Y),
+                new Vector2(GameConfig.VirtualResolution.X, GameConfig.VirtualResolution.Y)
             );
 
             var leftWallShape = new EdgeShape(
                 new Vector2(0, 0),
-                new Vector2(0, Game.Graphics.GraphicsDevice.Viewport.Height)
+                new Vector2(0, GameConfig.VirtualResolution.Y)
             );
 
             var rightWallShape = new EdgeShape(
-                new Vector2(Game.Graphics.GraphicsDevice.Viewport.Width, 0),
-                new Vector2(Game.Graphics.GraphicsDevice.Viewport.Width, Game.Graphics.GraphicsDevice.Viewport.Height)
+                new Vector2(GameConfig.VirtualResolution.X, 0),
+                new Vector2(GameConfig.VirtualResolution.X, GameConfig.VirtualResolution.Y)
             );
 
             var topWallShape = new EdgeShape(
                 new Vector2(0, 0),
-                new Vector2(0, Game.Graphics.GraphicsDevice.Viewport.Height)
+                new Vector2(0, GameConfig.VirtualResolution.Y)
             );
 
             // Fixtures
-            var fixture = _giftBody.CreateFixture(giftShape);
+            _giftFixture = _giftBody.CreateFixture(giftShape);
             var bottomWallFixture = _bottomWallBody.CreateFixture(bottomWallShape);
             var leftWallFixture = _leftWallBody.CreateFixture(leftWallShape);
             var rightWallFixture = _rightWallBody.CreateFixture(rightWallShape);
@@ -109,8 +137,12 @@ namespace XmasHell.Entities.Bosses.XmasGift
 
             if (InputManager.KeyDown(Keys.Space))
             {
-                _giftBody.ApplyLinearImpulse(new Vector2(1000, 10));
+                //_giftBody.ApplyLinearImpulse(new Vector2(1000, 10));
                 //_giftBody.ApplyAngularImpulse(10f);
+                var forceVector = new Vector2(0.5f, 0.5f);
+                var strength = 10f;
+                forceVector.Normalize();
+                _giftBody.ApplyForce(forceVector * strength);
             }
 
             _world.Step((float)gameTime.ElapsedGameTime.TotalSeconds);
@@ -126,7 +158,7 @@ namespace XmasHell.Entities.Bosses.XmasGift
             var projection = Matrix.CreateOrthographicOffCenter(0f, Game.GraphicsDevice.Viewport.Width, Game.GraphicsDevice.Viewport.Height, 0f, 0f, 1f);
             var view = Game.Camera.GetViewMatrix();
 
-            //_debugView.Draw(ref projection, ref view);
+            _debugView.Draw(ref projection, ref view);
         }
     }
 }
---
+++ Xmas-Hell/Xmas-Hell-Core/Physics/DebugView/DebugView.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FarseerPhysics.Dynamics;
+using Microsoft.Xna.Framework;
+
+namespace Xmas_Hell_Core.Physics.DebugView
+{
+    class DebugView
+    {
+        private DebugViewXna _debugView;
+
+        public DebugView(World _world, XmasHell.XmasHell game, float _ratio)
+        {
+            _debugView = new DebugViewXna(_world);
+            _debugView.AppendFlags(FarseerPhysics.DebugViewFlags.DebugPanel);
+            _debugView.DefaultShapeColor = Color.Red;
+            _debugView.SleepingShapeColor = Color.DarkRed;
+            _debugView.LoadContent(game.GraphicsDevice, game.Content);
+            _debugView.AppendFlags(FarseerPhysics.DebugViewFlags.Shape);
+            //_debugView.AppendFlags(FarseerPhysics.DebugViewFlags.PerformanceGraph);
+            //_debugView.AppendFlags(FarseerPhysics.DebugViewFlags.AABB);
+            //_debugView.AppendFlags(FarseerPhysics.DebugViewFlags.CenterOfMass);
+            //_debugView.AppendFlags(FarseerPhysics.DebugViewFlags.ContactNormals);
+            //_debugView.AppendFlags(FarseerPhysics.DebugViewFlags.ContactPoints);
+            //_debugView.AppendFlags(FarseerPhysics.DebugViewFlags.Controllers);
+            //_debugView.AppendFlags(FarseerPhysics.DebugViewFlags.Joint);
+            //_debugView.AppendFlags(FarseerPhysics.DebugViewFlags.PolygonPoints);
+            //
+            //_debugView.AppendFlags(FarseerPhysics.DebugViewFlags.PolygonPoints);
+
+
+            this.game = game;
+            this._ratio = _ratio;
+            Enabled = true;
+        }
+
+        public XmasHell.XmasHell game { get; set; }
+        public float _ratio { get; private set; }
+        public bool Enabled { get; set; }
+
+        public void Draw(ref Matrix projection, ref Matrix view)
+        {
+            if (Enabled) _debugView.RenderDebugData(ref projection, ref view);
+        }
+    }
+}
