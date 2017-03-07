f0433469a87e0b79bd2b2dbd309f1712b74f9c8d
Noxalus <noxalus@gmail.com>
2017-03-07 23:11:15 +0100

    Fix appveyor scripts.

--- Scripts/InstallDependencies.ps1
+++ Scripts/InstallDependencies.ps1
@@ -5,7 +5,6 @@ $zipPath = "$($env:APPVEYOR_BUILD_FOLDER)\xpkg.zip"
 7z x $zipPath | Out-Null
 Set-Content -path "$env:USERPROFILE\.xamarin-credentials" -value "xamarin.com,$env:XAMARIN_COOKIE"
 
-
 # Install MonoGame
 Write-Host "Installing MonoGame..."
 (New-Object Net.WebClient).DownloadFile('http://www.monogame.net/releases/v3.6/MonoGameSetup.exe', 'C:\MonoGameSetup.exe')
---
+++ Scripts/UpdateAndroidManifestVersion.ps1
@@ -0,0 +1,14 @@
+$ProjectPath = "$env:APPVEYOR_BUILD_FOLDER\$env:ANDROID_PROJECT_PATH"
+
+# Load the bootstrap file
+[xml] $xam = Get-Content -Path ($ProjectPath + "\Properties\AndroidManifest.xml")
+
+# Get the version from Android Manifest
+$version = Select-Xml -xml $xam  -Xpath "/manifest/@android:versionName" -namespace @{android="http://schemas.android.com/apk/res/android"}
+
+# Increment the version
+[double] $iVer = $version.Node.Value
+$version.Node.Value = "$env:APPVEYOR_BUILD_NUMBER"
+
+# Save the file
+$xam.Save($ProjectPath + "\Properties\AndroidManifest.xml")
