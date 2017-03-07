f0ba5ceadad6204682708f32a19be1b775524037
Noxalus <noxalus@gmail.com>
2017-03-07 23:17:01 +0100

    Try to fix install dependencies script for appveyor.

--- Scripts/InstallDependencies.ps1
+++ Scripts/InstallDependencies.ps1
@@ -1,12 +1,12 @@
 # Install Xamarin
-Write-Host "Installing Xamarin..."
+Write-Host ("Installing Xamarin...")
 $zipPath = "$($env:APPVEYOR_BUILD_FOLDER)\xpkg.zip"
 (New-Object Net.WebClient).DownloadFile('https://components.xamarin.com/submit/xpkg', $zipPath)
 7z x $zipPath | Out-Null
 Set-Content -path "$env:USERPROFILE\.xamarin-credentials" -value "xamarin.com,$env:XAMARIN_COOKIE"
 
 # Install MonoGame
-Write-Host "Installing MonoGame..."
+Write-Host ("Installing MonoGame...")
 (New-Object Net.WebClient).DownloadFile('http://www.monogame.net/releases/v3.6/MonoGameSetup.exe', 'C:\MonoGameSetup.exe')
 Invoke-Command -ScriptBlock {C:\MonoGameSetup.exe /S /v/qn}
 
