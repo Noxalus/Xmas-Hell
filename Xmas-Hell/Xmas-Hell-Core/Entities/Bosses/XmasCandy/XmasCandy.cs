efde8ed75d31332b5fdb2fa1fcae070e45600914
Noxalus <noxalus@gmail.com>
2017-07-15 23:44:29 +0200

    Update the Xmas Candy stretch out animation.

--- Xmas-Hell/Xmas-Hell-Core/Entities/Bosses/XmasCandy/XmasCandy.cs
+++ Xmas-Hell/Xmas-Hell-Core/Entities/Bosses/XmasCandy/XmasCandy.cs
@@ -27,6 +27,9 @@ namespace XmasHell.Entities.Bosses.XmasCandy
             base.InitializePhysics();
 
             Game.GameManager.CollisionWorld.AddBossHitBox(new SpriterCollisionCircle(this, "body.png"));
+
+            // TODO: Create 3 rectangle bounding boxes for the body.png part
+            // and one rectangle bounding box for each body2.png and body3.png parts
         }
 
         protected override void UpdateBehaviourIndex()
--- Xmas-Hell/Xmas-Hell-Core/Entities/Bosses/XmasCandy/XmasCandyBehaviour1.cs
+++ Xmas-Hell/Xmas-Hell-Core/Entities/Bosses/XmasCandy/XmasCandyBehaviour1.cs
@@ -1,7 +1,4 @@
-using System;
 using Microsoft.Xna.Framework;
-using MonoGame.Extended.Timers;
-using XmasHell.BulletML;
 
 namespace XmasHell.Entities.Bosses.XmasCandy
 {
