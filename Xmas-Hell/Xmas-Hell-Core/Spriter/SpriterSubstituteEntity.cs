1697b41845777b45653688e3bf39c3f5a49c71b3
Noxalus <noxalus@gmail.com>
2017-10-08 14:32:53 +0200

    Add a way to disable synchronization into SpriterSubstituteEntity.

--- Xmas-Hell/Xmas-Hell-Core/Spriter/SpriterSubstituteEntity.cs
+++ Xmas-Hell/Xmas-Hell-Core/Spriter/SpriterSubstituteEntity.cs
@@ -10,10 +10,16 @@ namespace XmasHell.Spriter
         private CustomSpriterAnimator _referenceAnimator;
         private SpriterDotNet.SpriterFile _replacedSpriterFile;
         private Size2 _replacedSpriterFileHalfSize;
+        private bool _synchronize = true;
 
         public BoundingRectangle BoundingRectangle()
         {
             return new BoundingRectangle(SubstituteAnimator.Position, _replacedSpriterFileHalfSize);
+        }
+
+        public void EnableSynchronization(bool value)
+        {
+            _synchronize = value;
         }
 
         public SpriterSubstituteEntity(string replacedPartFilename, CustomSpriterAnimator referenceAnimator, CustomSpriterAnimator substituteAnimator)
@@ -30,18 +36,21 @@ namespace XmasHell.Spriter
 
         public void Update(GameTime gameTime)
         {
-            // Synchronize current GUI button animator with the related dummy element from the Spriter file
-            var spriterDummyData = SpriterUtils.GetSpriterFileData(_replacedPartFilename, _referenceAnimator);
-
-            if (spriterDummyData != null)
+            if (_synchronize)
             {
-                var dummyPosition = new Vector2(spriterDummyData.X, -spriterDummyData.Y);
-                var dummyScale = new Vector2(spriterDummyData.ScaleX, spriterDummyData.ScaleY);
+                // Synchronize current GUI button animator with the related dummy element from the Spriter file
+                var spriterDummyData = SpriterUtils.GetSpriterFileData(_replacedPartFilename, _referenceAnimator);
 
-                SubstituteAnimator.Position = _referenceAnimator.Position + dummyPosition;
-                SubstituteAnimator.Rotation = -MathHelper.ToRadians(spriterDummyData.Angle);
-                SubstituteAnimator.Scale = dummyScale;
-                SubstituteAnimator.Color = new Color(SubstituteAnimator.Color, spriterDummyData.Alpha);
+                if (spriterDummyData != null)
+                {
+                    var dummyPosition = new Vector2(spriterDummyData.X, -spriterDummyData.Y);
+                    var dummyScale = new Vector2(spriterDummyData.ScaleX, spriterDummyData.ScaleY);
+
+                    SubstituteAnimator.Position = _referenceAnimator.Position + dummyPosition;
+                    SubstituteAnimator.Rotation = -MathHelper.ToRadians(spriterDummyData.Angle);
+                    SubstituteAnimator.Scale = dummyScale;
+                    SubstituteAnimator.Color = new Color(SubstituteAnimator.Color, spriterDummyData.Alpha);
+                }
             }
         }
     }
