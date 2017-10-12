6436400dacdcd2df133de34f941a676e263c2d7b
Noxalus <noxalus@gmail.com>
2017-10-12 08:03:42 +0200

    Add a project for iOS build.

---
+++ Xmas-Hell/Xmas-Hell-iOS/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Foundation;
+using UIKit;
+
+namespace Xmas_Hell_iOS
+{
+    [Register("AppDelegate")]
+    class Program : UIApplicationDelegate
+    {
+        private static XmasHell.XmasHell game;
+
+        internal static void RunGame()
+        {
+            game = new XmasHell.XmasHell();
+            game.Run();
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+
+        public override void FinishedLaunching(UIApplication app)
+        {
+            RunGame();
+        }
+    }
+}
