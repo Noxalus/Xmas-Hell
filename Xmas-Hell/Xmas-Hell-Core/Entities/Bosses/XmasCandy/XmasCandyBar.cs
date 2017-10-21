58af623f0daa5c7981090eafbcf6390e1d38c986
Noxalus <noxalus@gmail.com>
2017-10-21 14:52:31 +0200

    Update XmasCandyBar class to use a CustomSpriterAnimator instead of a Sprite.

--- Xmas-Hell/Xmas-Hell-Core/Entities/Bosses/XmasCandy/XmasCandyBar.cs
+++ Xmas-Hell/Xmas-Hell-Core/Entities/Bosses/XmasCandy/XmasCandyBar.cs
@@ -1,29 +1,37 @@
-﻿using System.Collections.Generic;
-using Microsoft.Xna.Framework;
-using MonoGame.Extended.Sprites;
+﻿using Microsoft.Xna.Framework;
 using XmasHell.Physics;
 using XmasHell.Physics.Collision;
+using XmasHell.Spriter;
+using XmasHell.Extensions;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace XmasHell.Entities.Bosses.XmasCandy
 {
-    class XmasCandyBar : AbstractDrawableEntity, IPhysicsEntity
+    class XmasCandyBar : IPhysicsEntity
     {
         private readonly Boss _boss;
         private CollisionConvexPolygon _boundingBox;
+        private CustomSpriterAnimator _animator;
+        public bool Destroyed = false;
+
+        public Vector2 Position()
+        {
+            return _animator.Position;
+        }
 
         public float Rotation()
         {
-            return Sprite.Rotation;
+            return _animator.Rotation;
         }
 
         public Vector2 Origin()
         {
-            return Sprite.Origin;
+            return _animator.Origin();
         }
 
         public Vector2 Scale()
         {
-            return Sprite.Scale;
+            return _animator.Scale;
         }
 
         public void TakeDamage(float damage)
@@ -31,38 +39,67 @@ namespace XmasHell.Entities.Bosses.XmasCandy
             _boss.TakeDamage(damage * 0.05f);
         }
 
-        public XmasCandyBar(Boss boss, Sprite sprite) : base(sprite)
+        public Vector2 ActionPointPosition()
+        {
+            var actionPointPosition = Vector2.Zero;
+
+            foreach (var pointData in _animator.FrameData.PointData)
+            {
+                if (pointData.Key.StartsWith("action_point"))
+                {
+                    var actionPoint = new Vector2(pointData.Value.X, -pointData.Value.Y);
+                    var rotatedActionPoint = MathExtension.RotatePoint(actionPoint, Rotation());
+
+                    actionPointPosition = Position() + rotatedActionPoint;
+                    break;
+                }
+            }
+
+            return actionPointPosition;
+        }
+
+        public XmasCandyBar(Boss boss, CustomSpriterAnimator animator)
         {
             _boss = boss;
+            _animator = animator;
 
-            // Physics
-            var bbWidth = sprite.TextureRegion.Width;
-            var startX = -sprite.TextureRegion.Width / 2f;
-            var startY = -sprite.TextureRegion.Height / 2f;
-            var vertices = new List<Vector2>()
-            {
-                new Vector2(startX, startY),
-                new Vector2(startX + bbWidth, startY),
-                new Vector2(startX + bbWidth, startY + sprite.TextureRegion.Height),
-                new Vector2(startX, startY + sprite.TextureRegion.Height)
-            };
+            _animator.Progress = 0;
+            _animator.Speed = 1;
+            _animator.AnimationFinished += AnimationFinishedHandler;
 
-            var relativePosition = new Vector2(0, sprite.TextureRegion.Height / 2f);
+            var candyBarCollisionBox = new SpriterCollisionConvexPolygon(_animator, "body2.png");
+            _boss.Game.GameManager.CollisionWorld.AddBossHitBox(candyBarCollisionBox);
 
-            _boundingBox = new CollisionConvexPolygon(this, relativePosition, vertices);
-            _boss.Game.GameManager.CollisionWorld.AddBossHitBox(_boundingBox);
+            _animator.Play("StretchInBorderMoving");
         }
 
         public void Dispose()
         {
             _boss.Game.GameManager.CollisionWorld.RemoveBossHitBox(_boundingBox);
+            _animator.AnimationFinished -= AnimationFinishedHandler;
+            Destroyed = true;
         }
 
-        public override void Update(GameTime gameTime)
+        public void StartStretchInAnimation()
         {
-            base.Update(gameTime);
+            _animator.Speed = 1;
+            _animator.Play("StretchInBorderHeight");
+        }
 
-            Sprite.Rotation += 0.01f;
+        private void AnimationFinishedHandler(string animationName)
+        {
+            if (animationName == "StretchInBorderWidth" || animationName == "StretchInBorderHeight")
+                Dispose();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            _animator.Update(gameTime.ElapsedGameTime.Milliseconds);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            _animator.Draw(spriteBatch);
         }
     }
 }
