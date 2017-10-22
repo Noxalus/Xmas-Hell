fcdb42ac5cd34aeb9344719a58144af1ffaecfa8
Noxalus <noxalus@gmail.com>
2017-10-22 15:22:19 +0200

    Update AbstractBossBehaviour to handle a single HP per pattern.

--- Xmas-Hell/Xmas-Hell-Core/Entities/Bosses/AbstractBossBehaviour.cs
+++ Xmas-Hell/Xmas-Hell-Core/Entities/Bosses/AbstractBossBehaviour.cs
@@ -6,22 +6,54 @@ namespace XmasHell.Entities.Bosses
     public abstract class AbstractBossBehaviour
     {
         protected Boss Boss;
+        protected float InitialBehaviourLife;
+        protected float CurrentBehaviourLife;
+        protected bool BehaviourEnded;
+
+        public float GetCurrentBehaviourLife()
+        {
+            return CurrentBehaviourLife;
+        }
+
+        public float GetLifePercentage()
+        {
+            return CurrentBehaviourLife / InitialBehaviourLife;
+        }
+
+        public bool IsBehaviourEnded()
+        {
+            return BehaviourEnded;
+        }
 
         protected AbstractBossBehaviour(Boss boss)
         {
             Boss = boss;
+            InitialBehaviourLife = GameConfig.BossDefaultBehaviourLife;
         }
 
         public virtual void Start()
         {
+            CurrentBehaviourLife = InitialBehaviourLife;
         }
 
         public virtual void Stop()
         {
         }
 
+        public virtual void TakeDamage(float amount)
+        {
+            CurrentBehaviourLife -= amount;
+        }
+
+        protected virtual void CheckBehaviourIsEnded()
+        {
+            if (CurrentBehaviourLife < 0)
+                BehaviourEnded = true;
+        }
+
         public virtual void Update(GameTime gameTime)
         {
+            CheckBehaviourIsEnded();
         }
 
         public virtual void Draw(SpriteBatch spriteBatch)
