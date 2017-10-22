a6e77089868abf040ecc3ff95b2e35c12c40ae75
Noxalus <noxalus@gmail.com>
2017-10-22 17:36:19 +0200

    Implement methods of the DesktopPreferencesEditor.

--- Xmas-Hell/Xmas-Hell-Core/PlayerData/Desktop/DesktopPreferencesEditor.cs
+++ Xmas-Hell/Xmas-Hell-Core/PlayerData/Desktop/DesktopPreferencesEditor.cs
@@ -1,6 +1,7 @@
 ﻿#if LINUX
 using System;
 using System.Collections.Generic;
+using System.Xml;
 
 namespace XmasHell.PlayerData.Desktop
 {
@@ -18,9 +19,103 @@ namespace XmasHell.PlayerData.Desktop
             Load();
         }
 
+        private void ReadXmlElement<T>(XmlReader reader, Dictionary<string, T> dictionary, Func<string, T> parseFunction)
+        {
+            XmlReader inner = reader.ReadSubtree();
+
+            while (inner.ReadToFollowing("element"))
+            {
+                reader.MoveToAttribute("key");
+                var key = reader.Value;
+                reader.MoveToAttribute("value");
+                var value = reader.Value;
+
+                dictionary.Add(key, parseFunction(value));
+            }
+        }
+
         private void Load()
         {
-            // TODO: Load preferences.xml file to fill dictionaries
+            try
+            {
+                using (XmlReader reader = XmlReader.Create("preferences.xml"))
+                {
+                    reader.ReadToFollowing("intDictionary");
+                    ReadXmlElement(reader, _intDictionnary, int.Parse);
+                    reader.ReadToFollowing("boolDictionary");
+                    ReadXmlElement(reader, _boolDictionnary, bool.Parse);
+                    reader.ReadToFollowing("floatDictionary");
+                    ReadXmlElement(reader, _floatDictionnary, float.Parse);
+                    reader.ReadToFollowing("longDictionary");
+                    ReadXmlElement(reader, _longDictionnary, long.Parse);
+                    reader.ReadToFollowing("stringDictionary");
+                    ReadXmlElement(reader, _stringDictionnary, (s) => s);
+
+                    // TODO: Handle string set
+                };
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private void WriteXmlElement<T>(XmlWriter xmlWriter, Dictionary<string, T> dictionary)
+        {
+            foreach (var pair in dictionary)
+            {
+                xmlWriter.WriteStartElement("element");
+                xmlWriter.WriteAttributeString("key", pair.Key);
+                xmlWriter.WriteAttributeString("value", pair.Value.ToString());
+                xmlWriter.WriteEndElement();
+            }
+        }
+
+        private void Save()
+        {
+            XmlWriterSettings ws = new XmlWriterSettings();
+            ws.Indent = true;
+            XmlWriter xmlWriter = XmlWriter.Create("preferences.xml", ws);
+
+            xmlWriter.WriteStartDocument();
+
+            xmlWriter.WriteStartElement("preferences");
+
+            xmlWriter.WriteStartElement("intDictionary");
+            WriteXmlElement(xmlWriter, _intDictionnary);
+            xmlWriter.WriteEndElement();
+
+            xmlWriter.WriteStartElement("boolDictionary");
+            WriteXmlElement(xmlWriter, _boolDictionnary);
+            xmlWriter.WriteEndElement();
+
+            xmlWriter.WriteStartElement("floatDictionary");
+            WriteXmlElement(xmlWriter, _floatDictionnary);
+            xmlWriter.WriteEndElement();
+
+            xmlWriter.WriteStartElement("longDictionary");
+            WriteXmlElement(xmlWriter, _longDictionnary);
+            xmlWriter.WriteEndElement();
+
+            xmlWriter.WriteStartElement("stringDictionary");
+            WriteXmlElement(xmlWriter, _stringDictionnary);
+            xmlWriter.WriteEndElement();
+
+            foreach (var pair in _stringSetDictionnary)
+            {
+                xmlWriter.WriteStartElement("element");
+                xmlWriter.WriteStartElement(pair.Key);
+
+                foreach (var str in pair.Value)
+                    xmlWriter.WriteString(str);
+
+                xmlWriter.WriteEndElement();
+                xmlWriter.WriteEndElement();
+            }
+
+            xmlWriter.WriteEndElement();
+            xmlWriter.WriteEndDocument();
+            xmlWriter.Close();
         }
 
         public bool Contains(string key)
@@ -91,57 +186,83 @@ namespace XmasHell.PlayerData.Desktop
         #region Common methods
         public void Apply()
         {
-            throw new NotImplementedException();
+            Save();
         }
 
         public IPreferencesEditor Clear()
         {
-            throw new NotImplementedException();
+            _intDictionnary.Clear();
+            _boolDictionnary.Clear();
+            _floatDictionnary.Clear();
+            _longDictionnary.Clear();
+            _stringDictionnary.Clear();
+            _stringSetDictionnary.Clear();
+
+            return this;
         }
 
         public bool Commit()
         {
-            throw new NotImplementedException();
+            return false;
         }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+
         }
 
         public IPreferencesEditor PutBoolean(string key, bool value)
         {
-            throw new NotImplementedException();
+            _boolDictionnary.Add(key, value);
+            return this;
         }
 
         public IPreferencesEditor PutFloat(string key, float value)
         {
-            throw new NotImplementedException();
+            _floatDictionnary.Add(key, value);
+            return this;
         }
 
         public IPreferencesEditor PutInt(string key, int value)
         {
-            throw new NotImplementedException();
+            _intDictionnary.Add(key, value);
+            return this;
         }
 
         public IPreferencesEditor PutLong(string key, long value)
         {
-            throw new NotImplementedException();
+            _floatDictionnary.Add(key, value);
+            return this;
         }
 
         public IPreferencesEditor PutString(string key, string value)
         {
-            throw new NotImplementedException();
+            _stringDictionnary.Add(key, value);
+            return this;
         }
 
         public IPreferencesEditor PutStringSet(string key, ICollection<string> values)
         {
-            throw new NotImplementedException();
+            _stringSetDictionnary.Add(key, values);
+            return this;
         }
 
         public IPreferencesEditor Remove(string key)
         {
-            throw new NotImplementedException();
+            if (_intDictionnary.ContainsKey(key))
+                _intDictionnary.Remove(key);
+            if (_floatDictionnary.ContainsKey(key))
+                _floatDictionnary.Remove(key);
+            if (_boolDictionnary.ContainsKey(key))
+                _boolDictionnary.Remove(key);
+            if (_longDictionnary.ContainsKey(key))
+                _longDictionnary.Remove(key);
+            if (_stringDictionnary.ContainsKey(key))
+                _stringDictionnary.Remove(key);
+            if (_stringSetDictionnary.ContainsKey(key))
+                _stringSetDictionnary.Remove(key);
+
+            return this;
         }
         #endregion
     }
