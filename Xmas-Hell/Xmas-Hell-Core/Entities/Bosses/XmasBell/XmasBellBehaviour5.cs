995a8cbdb34d1e0d4afc031004554a2bce25eaf8
Noxalus <noxalus@gmail.com>
2017-11-17 21:50:32 +0100

    Tweak Xmas Bell's 4th behaviour.

--- Xmas-Hell/Xmas-Hell-Core/Entities/Bosses/XmasBell/XmasBellBehaviour5.cs
+++ Xmas-Hell/Xmas-Hell-Core/Entities/Bosses/XmasBell/XmasBellBehaviour5.cs
@@ -22,15 +22,14 @@ namespace XmasHell.Entities.Bosses.XmasBell
 
         private void AnimationEventTriggered(string name)
         {
-            if (name == "hit-bottom")
+            if (name == "hit-bottom" || name == "hit-wall")
             {
-                Boss.Game.Camera.Shake(0.5f, 20);
-            }
-            else if (name == "hit-wall")
-            {
-                Boss.Game.Camera.Shake(0.25f, 10);
+                if (name == "hit-bottom")
+                    Boss.Game.Camera.Shake(0.5f, 100);
+                else
+                    Boss.Game.Camera.Shake(0.25f, 10);
 
-                var randomBulletCount = Boss.Game.GameManager.Random.Next(1, 5);
+                var randomBulletCount = Boss.Game.GameManager.Random.Next(5, 10);
                 var bounds = new Rectangle(
                     50, -100,
                     Boss.Game.ViewportAdapter.VirtualWidth - 50, 100
@@ -39,7 +38,7 @@ namespace XmasHell.Entities.Bosses.XmasBell
                 for (int i = 0; i < randomBulletCount; i++)
                 {
                     var patternPosition = Boss.Game.GameManager.GetRandomPosition(false, bounds);
-                    patternPosition.Y = -500;
+                    patternPosition.Y = -50;
 
                     Boss.TriggerPattern("XmasBell/pattern5", BulletML.BulletType.Type4, false, patternPosition);
                 }
@@ -60,6 +59,7 @@ namespace XmasHell.Entities.Bosses.XmasBell
             base.Stop();
 
             Boss.CurrentAnimator.AnimationFinished -= AnimationFinished;
+            Boss.CurrentAnimator.EventTriggered -= AnimationEventTriggered;
         }
 
         private void StartClapperPattern()
